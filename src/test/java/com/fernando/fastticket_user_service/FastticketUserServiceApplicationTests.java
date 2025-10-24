95ea02a39bb4944794744c7b6cdcf4e09f031748
Fernando <asialer05@hotmail.com>
2025-10-24 00:57:03 -0500

    Proyecto base

---
+++ src/test/java/com/fernando/fastticket_user_service/FastticketUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.fastticket_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastticketUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
